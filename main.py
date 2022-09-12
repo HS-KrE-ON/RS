b13477a782d7d2344c7840d8c4df13233077828d
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-12 22:14:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+"""
+Test implementation of a Restful API
+"""
+import os
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+
+api =   Api(app)
+
+@app.route('/nichtDieProvinzial')
+def nichtDieProvinzial():
+    programmierpojektnote = request.args.get('programmierpojektnote')
+
+    return '''<div>{}</div>'''.format(programmierpojektnote)
+
+
+class HelloWorld(Resource):
+    """
+    Test API Endpoint
+    """
+    def get(self):
+        """
+        Getter for hello world
+        """
+
+        data={"data": "Hi there, Programming Project!"}
+
+        return data
+
+class HelloEndpoint(Resource):
+    """
+    New Endpoint - CI / CD purpose
+    """
+    def get(self):
+        """
+        Getter for new Endpoint
+        """
+        data={"data": "Hi there from new Endpoint"}
+
+        return data
+
+class HelloBulti(Resource):
+    """
+    Test API Endpoint
+    """
+    def get(self):
+        """
+        Getter for hello world
+        """
+        data={"data": "Hi there, Moritz Bulthaub!"}
+
+        return data
+
+api.add_resource(HelloWorld,'/hello')
+api.add_resource(HelloEndpoint,'/endpoint')
+
+
+if __name__=='__main__':
+    cfg_port = os.getenv('PORT', "5000")
+
+
+    app.run(host="0.0.0.0", port=cfg_port)#, debug=True)
+#Test
