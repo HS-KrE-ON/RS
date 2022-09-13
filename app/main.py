c58e1ebcb826e418f59cfd7fb3eca04960607005
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-14 00:18:01 +0200

    logging implemented

--- app/main.py
+++ app/main.py
@@ -3,8 +3,12 @@ Test implementation of a Restful API
 """
 import os
 from website import create_app
+import logging
 
 app = create_app()
+
+app.logger.addHandler(logging.StreamHandler(os.system.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__=='__main__':
     cfg_port = os.getenv('PORT', "5000")
