34424660b4c116455cac48842d8bd61d18394e3c
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-13 09:12:37 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -34,9 +34,7 @@ class HelloEndpoint(Resource):
 
         return data
     
-api.add_resource(HelloWorld,'/hello')
-
-api.add_resource(HelloEndpoint,'/endpoint')    
+api.add_resource(HelloWorld,'/hello')   
 
 if __name__=='__main__':
     cfg_port = os.getenv('PORT', "5000")
