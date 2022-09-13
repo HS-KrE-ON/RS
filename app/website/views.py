3e9741f6a92b9124992eac4c1fc07c20f737a917
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-13 23:48:29 +0200

    try with /

--- app/website/views.py
+++ app/website/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/home')
 def home():
     """loads home.html file as a template to view"""
     return render_template("home")
