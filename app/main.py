d3c91faeaed70ba655f5da543366cfc6aca5e91a
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-13 09:59:50 +0200

    Anzeige der Daten schaffen

--- app/main.py
+++ app/main.py
@@ -34,9 +34,9 @@ class HelloEndpoint(Resource):
 
         return data
 
+api.add_resource(HelloWorld,'/hello')    
+
 if __name__=='__main__':
     cfg_port = os.getenv('PORT', "5000")
-
-
     app.run(host="0.0.0.0", port=cfg_port)#, debug=True)
 #Test
