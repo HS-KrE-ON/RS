5381306bba7206a82f03fac3f713e19a2c9d658c
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-13 17:59:46 +0200

    Aufbauen der Website mit html Test

--- app/main.py
+++ app/main.py
@@ -4,38 +4,9 @@ Test implementation of a Restful API
 import os
 from flask import Flask
 from flask_restful import Api, Resource
+from website import create_app
 
-app = Flask(__name__)
-
-api =   Api(app)
-
-class HelloWorld(Resource):
-    """
-    Test API Endpoint
-    """
-    def get(self):
-        """
-        Getter for hello world
-        """
-
-        data={"data": "Hallo, endgueltig klappt diese scheiss api mit dem richtigen Namen"}
-
-        return data
-
-class HelloEndpoint(Resource):
-    """
-    New Endpoint - CI / CD purpose
-    """
-    def get(self):
-        """
-        Getter for new Endpoint
-        """
-        data={"data": "Hi there from new Endpoint"}
-
-        return data
-
-api.add_resource(HelloWorld,'/hello')
-api.add_resource(HelloEndpoint,'/endpoint')
+app = create_app()
 
 if __name__=='__main__':
     cfg_port = os.getenv('PORT', "5000")
