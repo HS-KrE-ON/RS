accd5a3b73a2451f97696880a899521381ce97c6
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-14 00:55:30 +0200

    change to override innit

---
+++ app/app.py
@@ -0,0 +1,16 @@
+"""
+Test implementation of a Restful API
+"""
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """loads home.html file as a template to view"""
+    return render_template('home.html')
+
+if __name__=='__main__':
+    cfg_port = os.getenv('PORT', "5000")
+    app.run(host="0.0.0.0", port=cfg_port, debug=True)
