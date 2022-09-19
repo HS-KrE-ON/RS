085b6eb2a2d7eae51bda987b60d12e1dae1da578
ThienJ0 <113101698+ThienJ0@users.noreply.github.com>
2022-09-19 14:39:05 +0200

    Code_ReadCSV_3

--- recommendationsystem/system.py
+++ recommendationsystem/system.py
@@ -1,6 +1,7 @@
 """Import von movie_titles"""
 import pandas as pd
-df = pd.read_csv("https://raw.githubusercontent.com/HS-KrE-ON/RecommendationSystem/jannik/archive/movie_titles.csv",
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/HS-KrE-ON/RS/main/archive/movie_titles.csv",
                  encoding = "ISO-8859-1", on_bad_lines='skip')
 df1 = df.head(3)
 print(df1)
