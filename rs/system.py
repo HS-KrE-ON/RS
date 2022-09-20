6b692a20c66fee4cb326c17697664e9b7c3be948
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-20 22:07:15 +0200

    change to fiy syntax error

--- rs/system.py
+++ rs/system.py
@@ -4,4 +4,5 @@ df = pd.read_csv(
     "https://raw.githubusercontent.com/HS-KrE-ON/RS/main/archive/movie_titles.csv",
                  encoding = "ISO-8859-1", on_bad_lines='skip')
 df1 = df.head(10)
-print(df1.to_html())
+df2 = df1.to_html()
+print(df2)
