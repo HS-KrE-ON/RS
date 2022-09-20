c772dbcaaa803ba30634f98594c2910886566a0d
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-20 15:48:10 +0200

    Changes to show csv in heroku

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 Test implementation of a Restful API
 """
 import os
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,8 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """loads .html file as a template to view"""
-    statement = "hello world!"
-    return statement
+    df = pd.read_csv("https://raw.githubusercontent.com/HS-KrE-ON/RS/main/archive/movie_titles.csv",
+                 encoding = "ISO-8859-1", on_bad_lines='skip')
+    return df.to_html()
 
 @app.route('/home')
 def home():
