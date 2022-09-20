b0903cae5e7e3fc9863d93510d184fc6c25ace75
Alexander Katzorke <42177102+katzalex@users.noreply.github.com>
2022-09-20 23:22:49 +0200

    now the right changes

--- rs/system.py
+++ rs/system.py
@@ -1,8 +1,8 @@
+"""showing first lines of csv data"""
 import pandas as pd
 df = pd.read_csv(
     "https://raw.githubusercontent.com/HS-KrE-ON/RS/main/archive/movie_titles.csv",
                  encoding = "ISO-8859-1", on_bad_lines='skip')
 df1 = df.head(10)
 df2 = df1.to_html()
-def show():
-    print(df2)
+print(df2)
