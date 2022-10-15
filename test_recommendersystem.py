8508331792d16c6a07478f4dc70fd5ba3cfb5ac7
DustinMusic <111979534+DustinMusic@users.noreply.github.com>
2022-10-15 17:18:45 +0200

    Readjustment

--- test_recommendersystem.py
+++ test_recommendersystem.py
@@ -1,7 +1,6 @@
 '''Unit Test for Recommendations'''
 import unittest
 from static.recommendersystem import recommendate
-from static.script.js import submitMovies
 
 class TestRecommendation(unittest.TestCase):
     '''
@@ -26,13 +25,6 @@ class TestRecommendation(unittest.TestCase):
         '''Test if the output array is empty'''
         empty_list=[]
         output_list = recommendate(['Dinosaur Planet (2003)'])
-        self.assertNotEqual(output_list, empty_list)
-
-    def test_frontend(self):
-        '''Test if frontend is reachable'''
-        empty_list=[]
-        movies =['Dinosaur Planet (2003)']
-        output_list = submitMovies(movies)
         self.assertNotEqual(output_list, empty_list)
 
 if __name__ == '__main__':
